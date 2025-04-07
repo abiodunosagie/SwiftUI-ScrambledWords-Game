424d14894293262c68ccc5401a5f86c3e49eb858
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-07 14:06:07 +0100

    Initial Commit

---
+++ ScrambledWords/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrambledWords
+//
+//  Created by Abiodun Osagie on 07/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack{
+                Image("orange")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            }// VSTACK
+        }//: ZSTACK
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrambledWords/ScrambledWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambledWordsApp.swift
+//  ScrambledWords
+//
+//  Created by Abiodun Osagie on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrambledWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
